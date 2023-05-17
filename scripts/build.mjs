2c56587fe19511490b26cef3c9d87bc1f93b5ee3
Nic <951477037@qq.com>
2023-05-17 10:06:44 +0800

    chore: 优化脚本

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -16,43 +16,50 @@ await Promise.all([
     $`cd subapp-manage-module && npm run build`,
     $`cd subapp-qc-center && npm run build`,
     $`cd subapp-referral && npm run build`,
+    $`cd subapp-server-resource-manage && npm run build`,
+    $`cd subapp-ygt-portal && npm run build`,
 ]);
 
-// 在build目录下创建microapp-mb文件夹,并将main-app/main目录下的main文件夹复制到buildmicroapp-mb目录下
+// 在build目录下创建对应项目目录
 await Promise.all([
     await $`mkdir -p build/microapp-mb`,
     await $`mkdir -p build/microapp-manage`,
     await $`mkdir -p build/microapp-qc`,
-    // await $`mkdir -p build/microapp-mh`,
-    // await $`mkdir -p build/microapp-data-dock`,
+    await $`mkdir -p build/microapp-mh`,
+    await $`mkdir -p build/microapp-data-dock`,
     await $`mkdir -p build/microapp-referral`,
 ])
 
+// 复制主应用产物到对应项目目录
 await Promise.all([
     await $`cp -r main-app/main build/microapp-mb`,
     await $`cp -r main-app/main build/microapp-manage`,
     await $`cp -r main-app/main build/microapp-qc`,
-    // await $`cp -r main-app/main build/microapp-mh`,
-    // await $`cp -r main-app/main build/microapp-data-dock`,
+    await $`cp -r main-app/main build/microapp-mh`,
+    await $`cp -r main-app/main build/microapp-data-dock`,
     await $`cp -r main-app/main build/microapp-referral`,
 ])
 
 // 完成后删除 main-app/main
 await $`rm -rf main-app/main`;
 
+// 修改主应用config.js
 await Promise.all([
     await $`cp scripts/configs/慢病/config.js build/microapp-mb/main/js/config.js`,
     await $`cp scripts/configs/后台/config.js build/microapp-manage/main/js/config.js`,
     await $`cp scripts/configs/质量/config.js build/microapp-manage/main/js/config.js`,
-    // await $`cp scripts/configs/门户/config.js build/microapp-mh/main/js/config.js`,
-    // await $`cp scripts/configs/数据治理/config.js build/microapp-data-dock/main/js/config.js`,
+    await $`cp scripts/configs/门户/config.js build/microapp-mh/main/js/config.js`,
+    await $`cp scripts/configs/数据治理/config.js build/microapp-data-dock/main/js/config.js`,
     await $`cp scripts/configs/转诊/config.js build/microapp-referral/main/js/config.js`,
 ]);
 
+// 复制子应用产物到对应项目目录
 await Promise.all([
     await $`mv subapp-chronic-disease/chronicDisease build/microapp-mb`,
     await $`mv subapp-follow-up/followup build/microapp-mb`,
     await $`mv subapp-manage-module/manageModule build/microapp-manage`,
     await $`mv subapp-qc-center/qcCenter build/microapp-qc`,
     await $`mv subapp-referral/referral build/microapp-referral`,
+    await $`mv subapp-server-resource-manage/serverResourceManage build/microapp-data-dock`,
+    await $`mv subapp-ygt-portal/ygtPortal build/microapp-mh`,
 ]);
